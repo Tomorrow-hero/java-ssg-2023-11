4ed4254bb00dbe868e37842c270e1bd858ca06e2
Tomorrow-hero <seok550264@gmail.com>
2023-11-30 21:12:44 +0900

    게시물 상세페이지 구현 아직 날짜는 못함

--- src/Main.java
+++ src/Main.java
@@ -51,6 +51,29 @@ public class Main {
 
                     System.out.printf("번호:%d 제목:%s%n",article.id, article.title);
                 }
+            } else if (command.startsWith("article detail")) {
+                String[] commandBits = command.split(" ");
+                int id = Integer.parseInt(commandBits[commandBits.length - 1]);
+
+                Article foundArticle = null;
+
+                for (int i = 0; i < articles.size(); i++) {
+                    Article article = articles.get(i);
+
+                    if (article.id == id) {
+                        foundArticle = article;
+                        break;
+                    }
+                }
+
+                if(foundArticle == null) {
+                    System.out.printf("%d번 게시물은 존재하지 않습니다.%n", id);
+                }else {
+                    System.out.printf("번호 : %d %n", foundArticle.id);
+                    System.out.printf("날짜 : %s %n", "2023-00-00 00:00:00");
+                    System.out.printf("제목 : %s %n", foundArticle.title);
+                    System.out.printf("내용 : %s %n", foundArticle.body);
+                }
             } else {
                 System.out.printf("%s(은)는 존재히지 않는 명령어 입니다.%n", command);
             }
